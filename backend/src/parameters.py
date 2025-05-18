28e21fde04d0dd3ef16a3eea9014f140507e16ec
HernanRossi <hfdrossi@gmail.com>
2025-05-17 18:29:45 -0700

    Add GOOGLEAI_API_KEY to parameters; rename app to tabletop_trainer and update dependencies; enhance .gitignore for better cache handling

--- backend/src/parameters.py
+++ backend/src/parameters.py
@@ -2,4 +2,5 @@ import os
 
 
 JWT_SECRET = os.environ.get("JWT_SECRET")  # Set this in your env!
+GOOGLEAI_API_KEY = os.environ.get("GOOGLEAI_API_KEY")  # Set this in your env!
 JWT_ALGORITHM = "HS256"
